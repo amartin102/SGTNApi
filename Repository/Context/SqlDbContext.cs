f725fe18b4de06e829c06a5ddf152fb2b2b08cc7
amartin102 <martinezb19@gmail.com>
2025-09-25 20:37:03 -0500

    Agregar archivos de proyecto.

---
+++ Repository/Context/SqlDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Repository.Context
+{
+    public class SqlDbContext : DbContext
+    {
+        public string DefaultConnection { get; set; }
+
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            optionsBuilder.UseSqlServer(GetConnection());
+            //}
+        }
+
+        private string GetConnection()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            DefaultConnection = configuration.GetConnectionString("SqlServerConnectionString");
+            return DefaultConnection;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SGTNApi/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repository.Context;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Acceso a la configuración
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SqlDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("SqlServerConnectionString"));
+});
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var cs = configuration.GetConnectionString("SqlServerConnectionString");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
